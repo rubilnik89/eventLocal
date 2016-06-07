fc2402f715d3e7f7889d638e2d6f78d69a71bea6
Roman Zherebko <rubilnik89@mail.ru>
2016-06-07 17:00:50 +0300

    new work

---
+++ app/article.php
@@ -0,0 +1,21 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    //поля которые можно редактировать
+    protected $fillable = ['title', 'content'];
+}
+
+class stdClass extends Model
+{
+
+    
+}
+/**
+ * Created by PhpStorm.
+ * User: Роман
+ * Date: 27.05.2016
+ * Time: 11:02
+ */
